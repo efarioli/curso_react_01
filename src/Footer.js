bdc2294ec25bad1765d563a7925649ff75a3957c
efarioli <efarioli@gmail.com>
2020-08-17 09:31:55 +0100

    anadiendo decremento funcionalidad

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <p>&copy; Copyright 2020</p>
+      </footer>
+    )
+  }
+}
+
+export default Footer
---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>React SPA</h1>
+        <nav>
+          <a href="#">link</a>
+          <a href="#">link</a>
+          <a href="#">link</a>
+        </nav>
+      </header>
+    )
+  }
+}
+
+export default Header
